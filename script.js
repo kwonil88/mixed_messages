43807ac4f433ae874b41b974852f105894ae6667
Kwonil Jung <kwoniljung@Kwonils-MBP-2.attlocal.net>
2020-11-14 12:09:09 -0500

    final commit

--- script.js
+++ script.js
@@ -6,36 +6,33 @@ const messages = {
     jokes: ["I don't trust stairs; they are always up to something", "What do you call someone with no body and no nose? Nobody knows", "Why couldn't the bicycle stand by itself? Because it was two tired"]
 }
 
-const generateRandomNumber = num => {  
+const generateRandomNumber = num => {
     return Math.floor(Math.random() * num)
 }
-
-
 
 function generateRandomMessage() {
     let words = [];
     for (let category in messages) {
-    
-    let randomIndex = generateRandomNumber(messages[category].length)
-    
 
-    switch(category) {
-        case "sports_teams":
-            words.push(`your favorite sports team is ${messages[category][randomIndex]}`)
-        break;
-        case "motivation":
-            words.push(`Here is your motivational message: ${messages[category][randomIndex]}`)
-        break;
-        case "jokes":
-            words.push(`Dad joke of the day: ${messages[category][randomIndex]}`)
-        break
-        default:
-            words.push('There is not enough info.')
-    }
+        let randomIndex = generateRandomNumber(messages[category].length)
+
+
+        switch (category) {
+            case "sports_teams":
+                words.push(`your favorite sports team is ${messages[category][randomIndex]}`)
+                break;
+            case "motivation":
+                words.push(`Here is your motivational message: ${messages[category][randomIndex]}`)
+                break;
+            case "jokes":
+                words.push(`Dad joke of the day: ${messages[category][randomIndex]}`)
+                break
+            default:
+                words.push('There is not enough info.')
+        }
 
     }
-console.log(words);
+    console.log(words);
 }
-
 
 generateRandomMessage();
